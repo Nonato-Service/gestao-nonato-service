ca591cfb450ade0e75b555004807576a2e5da047
Nonato Service <nonato@example.com>
2026-05-07 22:57:02 +0100

    Atualização

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export const viewport: Viewport = {
   initialScale: 1,
   minimumScale: 1,
   maximumScale: 5,
-  themeColor: '#00ff00',
+  themeColor: '#12c96a',
   viewportFit: 'cover',
 }
 
@@ -50,7 +50,7 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="NONATO SERVICE" />
-        <meta name="theme-color" content="#00ff00" />
+        <meta name="theme-color" content="#12c96a" />
         <meta name="format-detection" content="telephone=no" />
       </head>
       <body suppressHydrationWarning>
